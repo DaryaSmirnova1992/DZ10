cbbf175d0114edb45d5292e25fe4ee5faef07b67
Darya <darecc@yandex.ru>
2022-09-13 18:46:49 +0400

    add Task_52

---
+++ Task_52/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+int [,] array = new int[3,3];
+for (int i=0; i<array.GetLength(0); i++)
+{
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+        
+        array[i,j] = new Random().Next(1,10);
+        Console.Write(array[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < 3; i++) 
+{
+    int srAr = 0;
+
+    for (int t = 0; t < 3; t++)
+    {
+        srAr += array[t, i];
+    }
+Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)srAr/3}");
+}
